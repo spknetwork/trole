4a3bed78d3cb9b63f2f3c19fa587e8261010c03b
Steven Ettinger <sjettingerjr@gmail.com>
2022-11-20 00:04:20 -0300

    cors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const config = require("./config");
 const express = require("express");
+const cors = require("cors");
 const api = express();
 var http = require("http").Server(api);
 const API = require("./api");
-console.log(config.dbcs)
+
 api.use(API.auth);
+api.use(cors())
 api.all("*", API.proxy);
 
 http.listen(config.port, function () {
   console.log(`API listening on port ${config.port}`);
 });
-
-
