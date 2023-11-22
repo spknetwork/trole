5850c119eaefb60a53cbc424dd9e635dda862ddc
disregardfiat <sjettingerjr@gmail.com>
2023-11-22 13:20:11 -0300

    ++

--- generate_key_pair.js
+++ generate_key_pair.js
@@ -2,7 +2,7 @@ const dhive = require('@hiveio/dhive');
 const Crypto = require('crypto')
 let opts = {};
 opts.addressPrefix = 'STM';
-const rando = Crypto.getRandomValues(new Uint16Array(32))
+const rando = Crypto.randomBytes(32).toString('hex');
 const ownerKey = dhive.PrivateKey.fromLogin('thestandarduser', rando, 'spk');
 console.log(ownerKey.toString(), ownerKey.createPublic(opts.addressPrefix).toString())
 process.exit()
