a6999316ef352677286f5e2b39aab7471c1dbf48
disregardfiat <sjettingerjr@gmail.com>
2025-04-19 02:32:31 -0300

    promo contract

--- api.js
+++ api.js
@@ -425,6 +425,7 @@ exports.storageStats = (req, res, next) => {
 
 exports.promo_contract = (req, res, next) => {
   const user = req.query.user;
+  console.log('promo',{ user })
   fetch(`${config.SPK_API}/@${user}`).then(rz => rz.json()).then(json => {
     if (!json.channels[config.account] && json.pubKey != 'NA') { //no contract
       var grant = parseInt(config.base_grant * 2), multiplier = 1
@@ -487,7 +488,7 @@ exports.promo_contract = (req, res, next) => {
 
 exports.contract = (req, res, next) => {
   const user = req.query.user;
-  console.log('promo',{ user })
+  console.log('contract',{ user })
   fetch(`${config.SPK_API}/@${user}`).then(rz => rz.json()).then(json => {
     if (!json.channels[config.account] && json.pubKey != 'NA') { //no contract
       var grant = config.base_grant, multiplier = 1
